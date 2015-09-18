711aed07edd6dac174866e93940373ea0bfe5565
osamaasaid <osamaasaid@gmail.com>
2015-09-17 21:26:42 -0700

    styling changes and a contact page

--- index.js
+++ index.js
@@ -9,16 +9,20 @@ var nodemailer = require('nodemailer');
 var ejsLayouts = require('express-ejs-layouts');
 
 
-
+//
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(ejsLayouts);
 app.use(express.static(__dirname));
 console.log('got here');
+app.use(ejsLayouts);
+
+
+
+
 
 
 app.get('/', function(req, res){
-   var url = 'http://api.nytimes.com/svc/topstories/v1/home.json';
+   var url = 'http://api.nytimes.com/svc/topstories/v1/world.json?api-key=';
   request({
     url: url,
     qs:{
@@ -26,8 +30,8 @@ app.get('/', function(req, res){
     }
   }, function(error, response, data){
       // console.log(data);
-      // res.render('layout.ejs', {results: JSON.parse(data).results[0]});
-      res.send(data);
+      res.render('main/index', {results: JSON.parse(data).results});
+       // res.send(data);
 
 
   });
@@ -36,6 +40,48 @@ app.get('/', function(req, res){
 
 
 
+app.post('/contact', function (req, res) {
+  // res.send(req.body);
+//   var mailBody = "";
+//   for(key in req.body){
+//     mailBody += key + ':\t' + req.body[key] + '\n\n';
+//   }
+//   res.send(mailBody);
+
+var transporter = nodemailer.createTransport({
+  service: 'yahoo',
+  auth: {
+    user: process.env.YAHOO_EMAIL,
+    pass: process.env.YAHOO_PASSWORD
+  }
+});
+transporter.sendMail({
+  from:'osamasapp@yahoo.com',
+  to:'osamaasaid@gmail.com',
+  subject:req.body.subject,
+  text:req.body.message
+},function(err,info){
+//   console.log('err',err);
+//   console.log('info',info);
+  // if (!req.body.message){
+  //   alert('Message connot be empty');
+  // }
+  // else{
+    res.redirect('/thanks');
+  // }
+  });
+
+});
+
+app.get('/thanks', function(req,res){
+  res.render('main/thanks')
+})
+
+app.get('/contact', function(req,res){
+  res.render('main/contact')
+});
 
 
-app.listen(3000);
+
+
+app.listen(process.env.PORT || 3000);
